6fde253c37c8f607f5699b56a0e2aee9e574dfe6
gustafsn <gustafsn@mail.gvsu.edu>
2019-11-30 18:03:33 -0500

    started making a class

---
+++ app/src/main/java/com/example/openglmusicvisualizer/firstVisualizer.java
@@ -0,0 +1,10 @@
+package com.example.openglmusicvisualizer;
+
+import android.media.audiofx.Visualizer;
+
+public class firstVisualizer {
+    Visualizer viz = new Visualizer(0);
+
+    viz.setEnabled(true);
+
+}
