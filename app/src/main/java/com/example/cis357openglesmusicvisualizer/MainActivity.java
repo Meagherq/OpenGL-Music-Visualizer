2241f97fa0835f58780027eb735f4076ed8c3f20
Meagherq <34166781+Meagherq@users.noreply.github.com>
2019-12-02 01:34:42 -0500

    Added RadioButton Functionality

--- app/src/main/java/com/example/cis357openglesmusicvisualizer/MainActivity.java
+++ app/src/main/java/com/example/cis357openglesmusicvisualizer/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.cis357openglesmusicvisualizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int SETTINGS_RESULT = 1;
+    //public static int SETTINGS_RESULT = 1;
     private Button displayButton;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
 
 
 
@@ -18,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayButton = findViewById(R.id.button);
-
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup = findViewById(R.id.radioGroup2);
         displayButton.setOnClickListener(v -> {
-            doConversion();
+            radioButton  = findViewById(radioGroup.getCheckedRadioButtonId());
+            String yourVote = radioButton.getText().toString();
+            Toast.makeText(MainActivity.this, "Selected Radio Button is:" + yourVote , Toast.LENGTH_LONG).show();
+            //startRenderer();
         });
     }
+
+    public void startRenderer() {
+        Intent intent = new Intent(this, MusicActivity.class);
+        startActivity(intent);
+    }
+
 }
 
 
